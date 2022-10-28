52fda3087bd74940cb20f9798cfeb1828416d9fd
loharganesh <lohar2159@gmail.com>
2022-10-28 15:56:44 +0530

    :wrench:

--- index.js
+++ index.js
@@ -9,47 +9,48 @@ const app = express();
 
 const upload = multer();
 
+//
 app.use(express.json());
 app.use(
-    express.urlencoded({
-        extended: true,
-    })
+	express.urlencoded({
+		extended: true,
+	})
 );
 app.use(
-    morgan(':method :url :status :res[content-length] - :response-time ms')
+	morgan(':method :url :status :res[content-length] - :response-time ms')
 );
 
 app.use(
-    cors({
-        origin: 'https://alterimage.netlify.app',
-    })
+	cors({
+		origin: 'https://alterimage.netlify.app',
+	})
 );
 
 app.get('/', (req, res) => {
-    res.status(200).send('Ok');
+	res.status(200).send('Ok');
 });
 // Default Route
 app.post('/', upload.array('files'), async (req, res) => {
-    try {
-        const { files } = req;
+	try {
+		const { files } = req;
 
-        const convertedToWebpFiles = [];
+		const convertedToWebpFiles = [];
 
-        for (let i = 0; i < files.length; i++) {
-            const result = await sharp(files[i].buffer).webp().toBuffer();
-            const base64Image = result.toString('base64');
-            convertedToWebpFiles.push({
-                name: removeExtension(files[i].originalname),
-                base64Image,
-            });
-        }
+		for (let i = 0; i < files.length; i++) {
+			const result = await sharp(files[i].buffer).webp().toBuffer();
+			const base64Image = result.toString('base64');
+			convertedToWebpFiles.push({
+				name: removeExtension(files[i].originalname),
+				base64Image,
+			});
+		}
 
-        return res.status(200).send(JSON.stringify(convertedToWebpFiles));
-    } catch (e) {
-        return res.status(500).send('Internal Server Error');
-    }
+		return res.status(200).send(JSON.stringify(convertedToWebpFiles));
+	} catch (e) {
+		return res.status(500).send('Internal Server Error');
+	}
 });
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('Server Started');
+	console.log('Server Started');
 });
